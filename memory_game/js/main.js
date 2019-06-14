30ea7d30f56b95b68aff2928d372a89069668533
James Daeho Kim <daeho@Daeho.local>
2019-06-14 14:31:12 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,29 @@
 console.log("Up and running!");
 
 
-var cards = ['queen', 'queen', 'king', 'king'];
+var cards = [
+	queen1 = {
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	}, 
+	queen2 = {
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"		
+	}, 
+	king1 = {
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	king2 = {
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	}
+];
+
 var cardsInPlay = [];
 
 function checkForMatch() {
@@ -9,14 +31,16 @@ function checkForMatch() {
 		console.log("You found a match!");
 	} else {
 		console.log("Sorry, try again.");
+		alert("Sorry, try again.");
+
 	}
 }
 
 function flipCard(cardId) {
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
-
-
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 
 	if (cardsInPlay.length === 2) {
 			checkForMatch();
