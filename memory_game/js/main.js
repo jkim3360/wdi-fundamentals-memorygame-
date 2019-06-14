2ebaf87042fae3445ef1ef8fdc07fa30d3a7d44e
James Daeho Kim <daeho@Daeho.local>
2019-06-14 10:22:13 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,17 +3,26 @@ console.log("Up and running!");
 
 var cards = ['queen', 'queen', 'king', 'king'];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
 
-if (cardsInPlay.length === 2) {
+function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	}	else {
-		alert("Sorry, try again.");
+		console.log("You found a match!");
+	} else {
+		console.log("Sorry, try again.");
 	}
 }
+
+function flipCard(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+
+
+
+	if (cardsInPlay.length === 2) {
+			checkForMatch();
+	}
+}
+
+flipCard(0)
+flipCard(2)
+
